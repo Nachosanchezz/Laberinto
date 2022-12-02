a181e09abcc0a63f008bfd7a72435ffefa63d683
Nacho <isanccal@myuax.com>
2022-12-02 21:20:46 +0100

    Cambio 3

--- Laberinto.py
+++ Laberinto.py
@@ -8,5 +8,13 @@ laberinto = [
 muro = ((0,1), (0,2), (0,3), (0,4), (1,1), (2,1), (2,3), (3,3), (4,0), (4,1), (4,2), (4,3))
 print("\n")
 
+def imprimirLaberinto(laberinto):
+    for i in laberinto:
+        for j in i:
+            print(j, end=" ")
+        print("\n")
+
+
+
 
 
